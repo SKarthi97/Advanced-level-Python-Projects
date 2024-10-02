b0320c8cad37b77fac61a84f600b2436cd7cd3d9
SKarthi97 <karthicksivakumar97@gmail.com>
2024-10-02 22:26:55 +0530

    Created a url for logout

--- financial-application/tracker/urls.py
+++ financial-application/tracker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="tracker/base.html"), name="tracker"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="tracker/logout.html"), name="logout"),
 ]
