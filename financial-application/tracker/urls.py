cc9afd17c318a0bff21c80f4f3810ef1fad7906c
SKarthi97 <karthicksivakumar97@gmail.com>
2024-10-02 22:12:55 +0530

    Login url defined

--- financial-application/tracker/urls.py
+++ financial-application/tracker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="tracker/base.html"), name="tracker"),
+    path('login/', auth_views.LoginView.as_view(template_name="tracker/login.html"), name='login'),
 ]
