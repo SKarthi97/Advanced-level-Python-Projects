4bbb6a9e1bb65876e116e68d76fcafee1a000ccc
SKarthi97 <karthicksivakumar97@gmail.com>
2024-10-09 23:53:43 +0530

    Created a model for  transactions

--- financial-application/tracker/models.py
+++ financial-application/tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    transaction_amount = models.DecimalField(max_digits=100, decimal_places=2)
+    transaction_type = models.ForeignKey('TransactionType', on_delete=models.SET_NULL, blank=True, null=True)
+    transaction_method = models.ForeignKey('TransactionMethod', on_delete=models.CASCADE)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.pk}"
