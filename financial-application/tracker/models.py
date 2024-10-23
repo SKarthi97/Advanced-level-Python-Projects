439d7b2b116605428d0d6ca1e7464d7e8f1a9d78
SKarthi97 <karthicksivakumar97@gmail.com>
2024-10-23 22:45:07 +0530

    Created a model for  bank account

--- financial-application/tracker/models.py
+++ financial-application/tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BankAccount(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bank_name = models.CharField(max_length=100)
+    bank_acc_number = models.IntegerField()
+    balance = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.balance}"
