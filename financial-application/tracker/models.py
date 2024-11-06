6cc192b692d835b793d904b23e8db70f41b9558e
SKarthi97 <karthicksivakumar97@gmail.com>
2024-11-06 22:42:52 +0530

    Updated the return value for traction method and type

--- financial-application/tracker/models.py
+++ financial-application/tracker/models.py
@@ -57,14 +57,14 @@ class TransactionMethod(models.Model):
     method = models.CharField(max_length=100)
 
     def __str__(self):
-        return method
+        return self.method
 
 class TransactionType(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     type = models.CharField(max_length=100)
 
     def __str__(self):
-        return type
+        return self.type
 
 class Budget(models.Model):
     budget = models.DecimalField(max_digits=100, decimal_places=2)
